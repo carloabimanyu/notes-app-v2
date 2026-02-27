5a5c6726828c8bc33517cbcdd015395ef310a206
carloabimanyu <carloabimanyu11@gmail.com>
2026-02-27 16:09:20 +0700

    feat(layout): implement Logo, Navbar, and ThemeToggle in MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,9 @@
 import { AppShell, Burger, Group, Text } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Logo from "../components/Logo";
+import ThemeToggle from "../components/ThemeToggle";
 
 function MainLayout() {
     const [opened, { toggle }] = useDisclosure();
@@ -16,7 +19,7 @@ function MainLayout() {
             }}
         >
             <AppShell.Header>
-                <Group h="100%" px="md">
+                <Group h="100%" px="md" justify="space-between">
                     <Burger
                         opened={opened}
                         onClick={toggle}
@@ -24,13 +27,15 @@ function MainLayout() {
                         size="sm"
                     />
 
-                    <Text fw={700}>Logo</Text>
+                   <Logo />
+
+                   <ThemeToggle />
                 </Group>
-                
+
             </AppShell.Header>
 
-            <AppShell.Navbar p="md">
-                <Text>Navbar</Text>
+            <AppShell.Navbar>
+                <Navbar />
             </AppShell.Navbar>
 
             <AppShell.Main>
