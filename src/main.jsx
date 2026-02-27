4fabbf1a91a0c3db7b438b50cc903dc2b23e2247
carloabimanyu <carloabimanyu11@gmail.com>
2026-02-27 09:57:50 +0700

    feat: setup initial routing structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <MantineProvider>
+            <App />
+        </MantineProvider>
+    </BrowserRouter>
 )
