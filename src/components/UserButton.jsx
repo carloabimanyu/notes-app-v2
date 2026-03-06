b7aa657211e1b86b8f3cda98ac27b711d5f45856
carloabimanyu <carloabimanyu11@gmail.com>
2026-03-06 15:23:57 +0700

    feat(components): show authed user name and email in UserButton

--- src/components/UserButton.jsx
+++ src/components/UserButton.jsx
@@ -1,24 +1,28 @@
 import { Avatar, Group, Text, UnstyledButton } from "@mantine/core";
 import classes from "./UserButton.module.css";
+import { useAuth } from "../contexts/AuthContext";
 
 function UserButton() {
+    const { authedUser } = useAuth();
+    const { name, email } = authedUser;
+
     return (
         <UnstyledButton className={classes.user}>
             <Group>
                 <Avatar
                     radius="xl"
-                    key={"User Name"}
-                    name={"User Name"}
+                    key={email}
+                    name={name}
                     color="initials"
                 />
 
                 <div style={{ flex: 1 }}>
                     <Text size="sm" fw={500}>
-                        User Name
+                        {name}
                     </Text>
 
                     <Text c="dimmed" size="xs">
-                        username@email.com
+                        {email}
                     </Text>
                 </div>
             </Group>
